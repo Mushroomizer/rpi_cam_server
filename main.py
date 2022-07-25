623edfe3c9d197fc99ad9547d4f1395ec77b9a2d
Cuan Otto <cuan.otto@iotnxt.com>
2022-07-25 23:24:37 +0200

    html response

--- main.py
+++ main.py
@@ -38,9 +38,7 @@ class StreamingHandler(server.BaseHTTPRequestHandler):
 
             self.send_response(200)
             self.send_header("Content-Type", "text/html")
-            self.send_header("Content-Length", len(content))
             self.end_headers()
-            self.wfile.write(content)
         elif self.path == "/stream.mjpg":
             self.send_response(200)
             self.send_header("Age", 0)
