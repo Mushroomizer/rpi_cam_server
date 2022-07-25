4743bbc3b60d361b1d1b096ecde6fa24f8dcad91
Cuan Otto <cuan.otto@gmail.com>
2022-07-25 20:20:03 +0200

    Relative paths

--- utils.py
+++ utils.py
@@ -2,12 +2,14 @@ import logging
 import os
 
 def get_html_from_page_name(pageName : str,directory : str = "./html") -> str:
+    working_dir = os.path.dirname(__file__)
     pageName = pageName.lstrip("/")
     pageName = pageName.rstrip("/")
     directory = directory.rstrip("/")
+    relative_directory = os.path.join(working_dir,str(directory))
+    mpath = os.path.join(relative_directory,pageName)
+    logging.warning("Looking for mpath: %s",str(mpath))
 
-    logging.info("Looking for mpath: %s/%s",str(directory),str(pageName))
-    mpath = os.path.join(directory,pageName)
     if(os.path.exists(mpath)):
         return open(mpath).read()
     logging.warning("Couldn't find .html for %s",pageName)
