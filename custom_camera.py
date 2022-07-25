8bfcc78cf0454e893f80e18a965db5a97015f54a
Cuan Otto <cuan.otto@iotnxt.com>
2022-07-25 23:01:21 +0200

    toggle profile

--- custom_camera.py
+++ custom_camera.py
@@ -1,22 +1,25 @@
 from picamera2 import Picamera2
 from picamera2.encoders import JpegEncoder
-from picamera2.outputs import FileOutput
+from picamera2.outputs import FileOutput, Quality
 
 from streaming_server import StreamingOutput
 import os
 
 class Cam:
-    def __init__(self) -> None:
-        # working_dir = os.path.dirname(__file__)
-        # self.tuning = Picamera2.load_tuning_file("camera_noir.json",os.path.join(working_dir, "./config"))
-        # self.tuning["rpi.agc"]["exposure_modes"]["normal"] = {"shutter": [100, 66666], "gain": [1.0, 8.0]}
-        # self.picam2 = Picamera2(tuning=self.tuning)
-        self.picam2 = Picamera2()
+    def __init__(self,useNoir:bool = False) -> None:
+        if(useNoir):
+            working_dir = os.path.dirname(__file__)
+            self.tuning = Picamera2.load_tuning_file("camera_noir.json",os.path.join(working_dir, "./config"))
+            self.tuning["rpi.agc"]["exposure_modes"]["normal"] = {"shutter": [100, 66666], "gain": [1.0, 8.0]}
+            self.picam2 = Picamera2(tuning=self.tuning)
+        else:
+            self.picam2 = Picamera2()
+        
         self.picam2.configure(self.picam2.create_video_configuration(main={"size": (1024, 768)}))
 
     def jpeg_streaming_output(self) -> StreamingOutput:
         output = StreamingOutput()
-        self.picam2.start_recording(JpegEncoder(), FileOutput(output),quality=Quality.LOW)
+        self.picam2.start_recording(JpegEncoder(), FileOutput(output),quality=Quality.VERY_LOW)
         return output
 
     def __del__(self):
--- main.py
+++ main.py
@@ -5,59 +5,78 @@
 # Note: needs simplejpeg to be installed (pip3 install simplejpeg).
 
 import logging
+import socketserver
 from custom_camera import Cam
-from streaming_server import StreamingServer,server
+from streaming_server import StreamingServer, server
 from utils import get_html_from_page_name
+
 
 class StreamingHandler(server.BaseHTTPRequestHandler):
 
+    def __init__(self, request: bytes, client_address: tuple[str, int], server: socketserver.BaseServer) -> None:
+        self.useNoir = False
+        self.setupCamera()
+        super().__init__(request, client_address, server)
+    
+    def setupCamera(self):
+        self.ccam = Cam(useNoir=self.useNoir)
+        self.output = self.ccam.jpeg_streaming_output()
+
     def do_GET(self):
-        if self.path == '/':
+        if self.path == "/":
             self.send_response(301)
-            self.send_header('Location', '/index.html')
+            self.send_header("Location", "/index.html")
             self.end_headers()
-        elif self.path == '/index.html':
-            content = get_html_from_page_name(self.path).encode('utf-8')
+        elif self.path == "/index.html":
+            content = get_html_from_page_name(self.path).encode("utf-8")
             self.send_response(200)
-            self.send_header('Content-Type', 'text/html')
-            self.send_header('Content-Length', len(content))
+            self.send_header("Content-Type", "text/html")
+            self.send_header("Content-Length", len(content))
             self.end_headers()
             self.wfile.write(content)
-        elif self.path == '/stream.mjpg':
+        elif self.path == "/toggle_profile":
+            if(self.useNoir):
+                self.useNoir = False
+            else:
+                self.useNoir = True
+            self.setupCamera()
+
             self.send_response(200)
-            self.send_header('Age', 0)
-            self.send_header('Cache-Control', 'no-cache, private')
-            self.send_header('Pragma', 'no-cache')
-            self.send_header('Content-Type', 'multipart/x-mixed-replace; boundary=FRAME')
+            self.send_header("Content-Type", "text/html")
+            self.send_header("Content-Length", len(content))
+            self.end_headers()
+            self.wfile.write(content)
+        elif self.path == "/stream.mjpg":
+            self.send_response(200)
+            self.send_header("Age", 0)
+            self.send_header("Cache-Control", "no-cache, private")
+            self.send_header("Pragma", "no-cache")
+            self.send_header(
+                "Content-Type", "multipart/x-mixed-replace; boundary=FRAME"
+            )
             self.end_headers()
             try:
                 while True:
-                    with output.condition:
-                        output.condition.wait()
-                        frame = output.frame
-                    self.wfile.write(b'--FRAME\r\n')
-                    self.send_header('Content-Type', 'image/jpeg')
-                    self.send_header('Content-Length', len(frame))
+                    with self.output.condition:
+                        self.output.condition.wait()
+                        frame = self.output.frame
+                    self.wfile.write(b"--FRAME\r\n")
+                    self.send_header("Content-Type", "image/jpeg")
+                    self.send_header("Content-Length", len(frame))
                     self.end_headers()
                     self.wfile.write(frame)
-                    self.wfile.write(b'\r\n')
+                    self.wfile.write(b"\r\n")
             except Exception as e:
                 logging.warning(
-                    'Removed streaming client %s: %s',
-                    self.client_address, str(e))
+                    "Removed streaming client %s: %s", self.client_address, str(e)
+                )
         else:
             self.send_error(404)
             self.end_headers()
 
-
-
-ccam = Cam()
-output = ccam.jpeg_streaming_output()
-
 try:
-    address = ('', 8000)
+    address = ("", 8000)
     server = StreamingServer(address, StreamingHandler)
     server.serve_forever()
 finally:
-    ccam.__del__()
-
+    logging.info("Done...")
